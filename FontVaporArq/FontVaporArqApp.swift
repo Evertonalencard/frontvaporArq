a6dd10dcc87084ba72ca562775782bb6d2743041
Éverton <evertonalencard24@gmail.com>
2026-06-02 16:29:20 -0300

    Initial Commit

---
+++ FontVaporArq/FontVaporArqApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontVaporArqApp.swift
+//  FontVaporArq
+//
+//  Created by Éverton Alencar de Lima on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FontVaporArqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
